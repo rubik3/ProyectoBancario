6438090524636d00dea6b032818dff21c192627c
Yeray <comodin26@gmail.com>
2019-04-16 12:07:24 +0200

    Creada la clase creditotest

---
+++ LosBancos/src/dominio/CreditoTest.java
@@ -0,0 +1,34 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CreditoTest {
+
+	@Test
+	void testRetirar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIngresar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPagoEnEstablecimiento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCredito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLiquidar() {
+		fail("Not yet implemented");
+	}
+
+}
