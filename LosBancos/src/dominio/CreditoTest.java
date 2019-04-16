6af877879fafc5dca664362e30d739638bcc0b34
Adrian Arpi <arpiadrian5@gmail.com>
2019-04-16 12:19:23 +0200

    Se ha añadido una clase test de Credito :"D

--- LosBancos/src/dominio/CreditoTest.java
+++ LosBancos/src/dominio/CreditoTest.java
@@ -7,28 +7,10 @@ import org.junit.Test;
 public class CreditoTest {
 
 	@Test
-	public void testRetirar() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testIngresar() {
-		fail("Not yet implemented");
+		//Para bailar la bamba....
 	}
 
-	@Test
-	public void testPagoEnEstablecimiento() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCredito() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLiquidar() {
-		fail("Not yet implemented");
-	}
+	
 
 }
